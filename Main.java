302ef793af25c8e04f6573cc8e0893c39ba8b56f
apostol-maxim <apostol_maxim@mail.ru>
2021-06-16 16:43:34 +0900

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
 // применение сортировки слиянием на java - merge(int[] A, int p, int q, int r)
 public class Main {
-    public static void main(String[] args) throws InterruptedException {
+    static final int MAX_VALUE = 10000;
 
+    public static void main(String[] args) {
         long start = System.currentTimeMillis();
         int[] arr = new int[]{5,2,4,6,1,3,2,6,7,4,9,7,8,11,21,42,3};
         int r = arr.length - 1;
@@ -27,8 +28,8 @@ public class Main {
         int n2 = r - q;
         int[] L = new int[n1 + 1];
         int[] R = new int[n2 + 1];
-        L[n1] = 10000;
-        R[n2] = 10000;
+        L[n1] = MAX_VALUE;
+        R[n2] = MAX_VALUE;
         System.arraycopy(A, p, L, 0, n1);
         for (int j = 0; j < n2; j++) {
             R[j] = A[q + j + 1];
